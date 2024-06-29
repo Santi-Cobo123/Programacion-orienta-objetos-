c95b015bf6277e1b6c5a007bf66ae7ea8e063564
Santiago Cobo <santiagocobo610@gmail.com>
2024-06-29 17:19:57 -0500

    semana5

--- semana 5/calcular el area.py
+++ semana 5/calcular el area.py
@@ -1,3 +1,6 @@
+# Este programa calcula el área de un rectángulo a partir de la base y altura ingresadas por el usuario.
+
+# Pedir al usuario que ingrese la base y la altura del rectángulo
 def calculate_rectangle_area():
     """
     Calcula el área de un rectángulo.
@@ -12,3 +15,5 @@ def calculate_rectangle_area():
 
 if __name__ == "__main__":
     calculate_rectangle_area()
+
+
